d5f8381d7a3151868c4377b68e1d0df23ec492bc
Romina Uicab <rominauicab1@gmail.com>
2022-02-20 21:40:57 -0500

    commit 1

--- app/src/main/java/com/example/climaprac_app/MainActivity.kt
+++ app/src/main/java/com/example/climaprac_app/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
 
     var twCiudad:TextView? = null
